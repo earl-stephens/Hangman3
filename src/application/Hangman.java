426324228aebcf7e4356d8cb1c2ad143f5f46931
earl-stephens <stephensearl@hotmail.com>
2022-01-03 17:43:55 -0700

    Prompt user to enter a guess
    Closes #Card4

--- src/application/Hangman.java
+++ src/application/Hangman.java
@@ -1,12 +1,26 @@
 package application;
 
+import java.util.Scanner;
+
 public class Hangman {
+	
 	RandomWord word = new RandomWord();
+	Scanner scanner = new Scanner(System.in);
+	
+	char guessedLetter;
 	String wordWithLetters;
 	
 	public void run() {
 		word.selectWord();
 		displayWord();
+		getPlayerInput();
+	}
+	
+	public void getPlayerInput() {
+		System.out.print("Enter your guess: ");
+		String input = scanner.nextLine();
+		guessedLetter = input.charAt(0);
+		System.out.println(guessedLetter);
 	}
 	
 	public void displayWord() {
