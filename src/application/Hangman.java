9eb0cbe98fc59dd62971b26fc648008fdc444ed0
earl-stephens <stephensearl@hotmail.com>
2022-01-03 17:25:51 -0700

    Display letters in the word
    Closes #Card3

--- src/application/Hangman.java
+++ src/application/Hangman.java
@@ -2,9 +2,16 @@ package application;
 
 public class Hangman {
 	RandomWord word = new RandomWord();
+	String wordWithLetters;
 	
 	public void run() {
 		word.selectWord();
+		displayWord();
+	}
+	
+	public void displayWord() {
+		wordWithLetters = word.displayTheWord();
+		System.out.println(wordWithLetters);
 	}
 	
 	public void close() {
